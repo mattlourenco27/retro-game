ffca4669eca2ae11c7ad35798abcc864347df419
mattlourenco27 <matt.lourenco27@gmail.com>
2020-05-15 17:24:17 -0400

    Added horizontal collisions with the paddle and ball

--- game-logic.js
+++ game-logic.js
@@ -17,6 +17,26 @@ function Ball() {
     context.beginPath();
     context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
     context.fill();
+  },
+  //handles collisions with the borders of the court
+  this.handleBorders = function() {
+    if(ball.x >= court.width) {
+      score.player += 1;
+      ball.x = court.width / 2;
+      ball.y = court.height / 2;
+    } else if(ball.x <= 0) {
+      score.ai += 1;
+      ball.x = court.width / 2;
+      ball.y = court.height / 2;
+    }
+
+    if(ball.y + ball.radius >= court.height) {
+      ball.vel_y = -ball.vel_y;
+      ball.y = court.height - ball.radius - 1;
+    } else if(ball.y - ball.radius <= 0) {
+      ball.vel_y = -ball.vel_y;
+      ball.y = ball.radius + 1;
+    }
   }
 }
 
@@ -27,10 +47,28 @@ function Paddle(x) {
   this.width = court.width / 30;
   this.x = x;
   this.y = court.height / 2;
+  this.top_edge = function() {return this.y - this.height / 2;}
+  this.bot_edge = function() {return this.y + this.height / 2;}
+  this.l_edge = function() {return this.x - this.width / 2;}
+  this.r_edge = function() {return this.x + this.width / 2;}
   this.draw = function() {
     context.beginPath();
     context.rect(this.x - this.width / 2, this.y - this.height / 2, this.width, this.height);
     context.fill();
+  },
+  //handles collisions with the ball
+  this.handleCollision = function() {
+    //handle side edge collisions
+    if(ball.y >= this.top_edge() && ball.y <= this.bot_edge()) {
+      if(ball.x + ball.radius >= this.l_edge() && ball.x + ball.radius <= this.r_edge()) {
+        ball.vel_x = -ball.vel_x;
+        ball.x -= 1;
+      }
+      if(ball.x - ball.radius >= this.l_edge() && ball.x - ball.radius <= this.r_edge()) {
+        ball.vel_x = -ball.vel_x;
+        ball.x += 1;
+      }
+    }
   }
 }
 
@@ -47,7 +85,7 @@ var score = {
 }
 
 ball.vel_x = 5;
-ball.vel_y = 5;
+ball.vel_y = 0;
 
 // begin the game loop running at 60fps
 setInterval(game_loop, 1000 / 60);
@@ -70,23 +108,10 @@ function game_loop() {
 
   //draw a ball
   ball.draw();
-  if(ball.x >= court.width) {
-    score.player += 1;
-    ball.x = court.width / 2;
-    ball.y = court.height / 2;
-  } else if(ball.x <= 0) {
-    score.ai += 1;
-    ball.x = court.width / 2;
-    ball.y = court.height / 2;
-  }
 
-  if(ball.y + ball.radius >= court.height) {
-    ball.vel_y = -ball.vel_y;
-    ball.y = court.height - ball.radius - 1;
-  } else if(ball.y - ball.radius <= 0) {
-    ball.vel_y = -ball.vel_y;
-    ball.y = ball.radius + 1;
-  }
+  ball.handleBorders();
+  opponent_paddle.handleCollision();
+  player_paddle.handleCollision();
 
   ball.x += ball.vel_x;
   ball.y += ball.vel_y;
