e96bbf0ec9c74b1f01525f9988ecf6930dc66a5f
mattlourenco27 <matt.lourenco27@gmail.com>
2020-05-15 17:03:30 -0400

    Added the game loop that draws the game 60 times per second

--- game-logic.js
+++ game-logic.js
@@ -1,61 +1,95 @@
-function begin() {
-  var court = document.getElementById("court");
-  var context = court.getContext("2d");
 
-  var colour = "#FFFFFFFF";
-  context.strokeStyle = colour;
+var court = document.getElementById("court");
+var context = court.getContext("2d");
+
+var colour = "#FFFFFFFF";
+context.strokeStyle = colour;
+context.fillStyle = colour;
+context.lineWidth = 2;
+
+function Ball() {
+  this.radius = 20;
+  this.x = court.width / 2;
+  this.y = court.height / 2;
+  this.vel_x = 0;
+  this.vel_y = 0;
+  this.draw = function() {
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    context.fill();
+  }
+}
+
+var ball = new Ball();
+
+function Paddle(x) {
+  this.height = court.height / 4;
+  this.width = court.width / 30;
+  this.x = x;
+  this.y = court.height / 2;
+  this.draw = function() {
+    context.beginPath();
+    context.rect(this.x - this.width / 2, this.y - this.height / 2, this.width, this.height);
+    context.fill();
+  }
+}
+
+var player_paddle = new Paddle(court.width / 60);
+var opponent_paddle = new Paddle(court.width * 59 / 60);
+
+var score = {
+  player: 0,
+  ai: 0,
+  px: court.width / 4,
+  py: court.height / 5,
+  ax: court.width * 3 / 4,
+  ay: court.height / 5
+}
+
+ball.vel_x = 5;
+ball.vel_y = 5;
+
+// begin the game loop running at 60fps
+setInterval(game_loop, 1000 / 60);
+
+
+function game_loop() {
+  context.beginPath();
+  context.fillStyle = "#000000FF";
+  context.rect(0, 0, court.width, court.height);
+  context.fill();
   context.fillStyle = colour;
-  context.lineWidth = 2;
-
-  function Ball() {
-    this.radius = 20;
-    this.x = court.clientWidth / 2;
-    this.y = court.clientHeight / 2;
-    this.vel_x = 0;
-    this.vel_y = 0;
-    this.draw = function() {
-      context.beginPath();
-      context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-      context.fill();
-    }
-  }
-
-  var ball = new Ball();
-
-  function Paddle(x) {
-    this.height = court.clientHeight / 4;
-    this.width = court.clientWidth / 30;
-    this.x = x;
-    this.y = court.clientHeight / 2;
-    this.draw = function() {
-      console.log(this.x);
-      context.beginPath();
-      context.rect(this.x - this.width / 2, this.y - this.height / 2, this.width, this.height);
-      context.fill();
-    }
-  }
-
-  var player_paddle = new Paddle(court.clientWidth / 60);
-  var opponent_paddle = new Paddle(court.clientWidth * 59 / 60);
-
-  var score = {
-    player: 0,
-    px: court.clientWidth / 4,
-    py: court.clientHeight / 5,
-    ai: 0,
-    ax: court.clientWidth * 3 / 4,
-    ay: court.clientHeight / 5
-  }
 
   // draw the middle line
+  context.beginPath();
   context.setLineDash([30, 30]);
-  context.moveTo(court.clientWidth / 2, 0);
-  context.lineTo(court.clientWidth / 2, court.clientHeight);
+  context.moveTo(court.width / 2, 0);
+  context.lineTo(court.width / 2, court.height);
   context.stroke();
   context.setLineDash([]);
 
   //draw a ball
   ball.draw();
+  if(ball.x >= court.width) {
+    score.player += 1;
+    ball.x = court.width / 2;
+    ball.y = court.height / 2;
+  } else if(ball.x <= 0) {
+    score.ai += 1;
+    ball.x = court.width / 2;
+    ball.y = court.height / 2;
+  }
+
+  if(ball.y + ball.radius >= court.height) {
+    ball.vel_y = -ball.vel_y;
+    ball.y = court.height - ball.radius - 1;
+  } else if(ball.y - ball.radius <= 0) {
+    ball.vel_y = -ball.vel_y;
+    ball.y = ball.radius + 1;
+  }
+
+  ball.x += ball.vel_x;
+  ball.y += ball.vel_y;
 
   //draw the player-paddle
   player_paddle.draw();
@@ -68,9 +102,3 @@ function begin() {
   context.fillText(score.player, score.px, score.py);
   context.fillText(score.ai, score.ax, score.ay);
 }
-
-begin();
-
-//debugging
-var c = document.getElementById("court");
-var ctx = court.getContext("2d");
